c762162342a5caada05e442fbef702241d495627
Nícolas Fray <nicopfray@gmail.com>
2021-05-21 11:11:30 -0300

    Bola adicionada

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,27 +6,25 @@ public class PlayerCamera : MonoBehaviour
 {
 	public GameObject FollowTarget;
 	GameObject CameraDummy;
-	//variávies para ajeitar o funcionamento da camera
+	
 	public float adjustY, transformAdjustY, adjustDistance, adjustRotation;
 	
     // Start is called before the first frame update
     void Start()
     {
-		//cria um objeto novo pra usar na rotação da camera
         CameraDummy = new GameObject("CamDummy");
-		FollowTarget.GetComponent<PlayerMovement>().SetCameraDummy(CameraDummy);
+		//FollowTarget.GetComponent<script>().SetDummyCam(CameraDummy);
     }
 
     // Update is called once per frame
     void Update()
     {
-		//move o dummy
 		CameraDummy.transform.position = FollowTarget.transform.position;
-		//move a camera
+		
 		transform.position = CameraDummy.transform.position - CameraDummy.transform.forward * adjustDistance + Vector3.up * transformAdjustY;
-		//rotaciona a camera
+		
         transform.LookAt(FollowTarget.transform.position + Vector3.up * adjustY);
-		//rotaciona o dummy, que muda a rotação e posição da camera
+		
 		CameraDummy.transform.Rotate(0, Input.GetAxis("Mouse X") * adjustRotation, 0);
     }
 }
