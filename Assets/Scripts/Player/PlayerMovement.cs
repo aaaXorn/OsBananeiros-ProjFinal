465ad687715dea6972e2df7897be0e3f6f6d664b
aaaXorn <lcrs.rp@gmail.com>
2021-06-01 10:16:17 -0300

    reset de scene quando o player morre, aumento de 24% do meu ódio por Raycast

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -152,6 +153,6 @@ public class PlayerMovement : MonoBehaviour
 	{
 		dying = true;
 		yield return new WaitForSeconds(deathTime);
-		//reload scene
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 }
