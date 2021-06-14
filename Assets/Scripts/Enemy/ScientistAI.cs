5b22ac8e3f94979ec47c5f49d4319ff5ec0c90b3
aaaXorn <lcrs.rp@gmail.com>
2021-06-14 14:31:31 -0300

    esboço código do boss

---
+++ Assets/Scripts/Enemy/ScientistAI.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScientistAI : MonoBehaviour
+{
+	/*
+	//script de HP do boss
+	public ScientistHP SHP;
+	
+	//variáveis de cooldown
+	public float timerCD, maxCD;
+	
+	//variáveis de morte do boss
+	public float deathTimer;
+	
+	//patterns do boss
+	public enum Pattern
+	{
+		CD,
+		Dead,
+	}
+	//pattern atual do boss
+	public Pattern currentPatt;
+	*/
+	
+	void Start()
+	{
+		
+	}
+	
+	void FixedUpdate()
+	{
+		/*
+		switch(currentPatt)
+		{
+			case Pattern.CD:
+				CD();
+				break;
+			
+			case Pattern.Dead:
+				deathTimer -= Time.deltaTime;
+				if(deathTimer <= 0)
+				{
+					Dead();
+				}
+				break;
+		}
+		*/
+	}
+	
+	/*
+	void CD()
+	{
+		
+	}
+	*/
+	
+	/*
+	void Dead()
+	{
+		
+	}
+	*/
+}
---
+++ Assets/Scripts/Enemy/ScientistHP.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScientistHP : MonoBehaviour
+{
+	/*
+	//script de AI do boss
+	public ScientistAI SAI;
+	
+	//vida do boss
+	public int HP;
+	*/
+	
+	void Start()
+	{
+		
+	}
+	
+	/*
+	void TakeDamage()
+	{
+		HP--;
+		
+		if(HP <= 0)
+			SAI.currentPatt = ScientistAI.Pattern.Dead;
+	}
+	*/
+}
